d2507f35ddaf39f85cbd91a7f2e9eb29a1ef4bb3
Master Of None <nezar.ras@gmail.com>
2020-04-10 12:28:01 +0300

    Corrected minor mistakes; Addition of two function;

--- main.c
+++ main.c
@@ -31,7 +31,29 @@ double covariance(double row1[], double row2[], int rowLength ) {
 
        }
 
-int main(int argc, char *argv[], double sum) {
+void covarianceMatrix(double ** inputMatrix, double ** outputMatrix, int rowLength) {
+
+    int p;
+    int j;
+    for(p = 0; p < (rowLength); p++);
+        for(j = 0; j < (rowLength); j++);
+            outputMatrix[p][j] = covariance(inputMatrix[p] , inputMatrix[j], rowLength);
+}
+
+
+void outputMatrixToFile(double ** outputMatrix, int * outputMatrixDimension, FILE *  outputFile) {
+
+    int rowsAndColumns = fwrite(outputMatrixDimension, sizeof(int), 2, outputFile);
+    assert(rowsAndColumns == 2);
+    int l;
+    for(l = 0; l < (outputMatrixDimension[0]); l++);
+        int toFileByRow = fwrite(outputMatrix[l], sizeof(double), outputMatrixDimension[0], outputFile);
+        assert(toFileByRow == outputMatrixDimension[0]);
+}
+
+
+
+int main(int argc, char *argv[]) {
 
     FILE *file = fopen(argv[1], "r");
     assert(file != NULL);
@@ -40,11 +62,11 @@ int main(int argc, char *argv[], double sum) {
     int numberOfParameters = fread(matrixDimension, sizeof(int), 2, file);
     assert(numberOfParameters == 2);
 
-    int rowLength = matrixDimension[1];    /// @todo find out which one is column/row.
-    int columnLength = matrixDimension[0]; /// Verified
+    int rowLength = matrixDimension[1];
+    int columnLength = matrixDimension[0];
 
-    double ** matrix = (double *) malloc(rowLength * sizeof(double));///Double Array
-    double ** outputMatrix = (double *) malloc(rowLength * sizeof(double));
+    double ** matrix = (double **) malloc(rowLength * sizeof(double));///Double Array
+    double ** outputMatrix = (double **) malloc(rowLength * sizeof(double));
     int i;
     for(i = 0; i < rowLength; i++){
         matrix[i] = (double *) malloc(columnLength * sizeof(double));
@@ -57,24 +79,17 @@ int main(int argc, char *argv[], double sum) {
     fclose(file);
 
    /// Function Standardize Given Matrix. Uses Covariance function.
-    int p;
-    int j;
-    for(p = 0; p < (rowLength); p++);
-        for(j = 0; j < (rowLength); j++);
-        outputMatrix[p][j] = covariance(matrix[p] , matrix[j], rowLength);
+   covarianceMatrix(matrix, outputMatrix, rowLength);
 
     /// Write outputMatrix to File
-    FILE *outputFile = fopen(argv[2], "r");
+    FILE *outputFile = fopen(argv[2], "w");
     assert(outputFile != NULL);
     int outputMatrixDimension = {rowLength, rowLength};
-    int rowsAndColumns = fwrite(outputMatrixDimension, sizeof(int), 2, outputFile);
-    assert(rowsAndColumns == 2);
-    int l;
-    for(l = 0; l < (rowLength); l++);
-        int toFileByRow = fwrite(outputMatrix[l], sizeof(double), rowLength, outputFile);
-        assert(toFileByRow == rowLength);
+    outputMatrixToFile(outputMatrix, outputMatrixDimension, outputFile);
 
 
+    free(matrix);
+    free(outputMatrix);
 
     return 0;
 }
