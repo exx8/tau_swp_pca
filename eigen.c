23dba9ae866886aaa5fb900bb37072b20dbaa590
exx8 <exx8eran@gmail.com>
2020-04-21 10:24:15 +0300

    generate random vector

--- eigen.c
+++ eigen.c
@@ -1,5 +1,7 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 FILE *openInputFile(char *const *argv) {
     FILE *input = fopen(argv[1], "r");
@@ -13,15 +15,29 @@ void getDimension(FILE *input, int *dimension) {
     assert(readStatus == 2);
 }
 
+double *getVector(int size) {
+    double *vector = (double *) calloc(size, sizeof(double));
+    const double *end = vector + size;
+    double *current = vector;
+    for (; current != end; current++)
+        *current = rand();
+    return vector;
+}
+
 int main(int argc, char *argv[]) {
     FILE *input;
     int dimension[2] = {0, 0};
     int readStatus = 0;
+    double *b0; /* iteration vector*/
+
     assert(argc == 2);
+    srand(time(NULL));
+
 
     input = openInputFile(argv);
-
     getDimension(input, dimension);
+    b0 = getVector(dimension[0]);
+    free(b0);
     fclose(input);
 }
 
