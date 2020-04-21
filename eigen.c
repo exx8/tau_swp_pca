54718e8e252bd4506b7b56d7036c1ac122b83405
exx8 <exx8eran@gmail.com>
2020-04-21 09:58:58 +0300

    read dimension

--- eigen.c
+++ eigen.c
@@ -7,12 +7,21 @@ FILE *openInputFile(char *const *argv) {
     return input;
 }
 
+void getDimension( FILE *input,  int *dimension) {
+    int readStatus;
+    readStatus=fread(dimension, sizeof(int), 2, input);
+    assert(readStatus==2);
+}
+
 int main(int argc, char *argv[]) {
     FILE *input;
+    int dimension[2] = {0, 0};
+    int readStatus=0;
     assert(argc == 2);
 
     input = openInputFile(argv);
 
-
+    getDimension(input, dimension);
+    fclose(input);
 }
 
